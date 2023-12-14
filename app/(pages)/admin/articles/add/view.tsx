474754bfd9a18a6522d1ea4e5767124f279dc00c
Philix27 <philixbob@gmail.com>
2023-12-14 09:38:48 +0100

    added page layout comps and admin articles page

---
+++ app/(pages)/admin/articles/add/view.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function View() {
+  return (
+    <div>
+      <div>
+        <input type={"text"} placeholder="title" />
+      </div>
+    </div>
+  );
+}
--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,6 +1,3 @@
-import "./lib/styles/index.scss";
-import type { Metadata } from "next";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div>{children}</div>;
 }
