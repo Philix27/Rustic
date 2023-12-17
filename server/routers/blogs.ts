b9f97e966804d30754284d63b4902c3768207d79
Philix27 <philixbob@gmail.com>
2023-12-17 10:12:29 +0100

    blog trpc router added

--- server/routers/blogs.ts
+++ server/routers/blogs.ts
@@ -1,14 +1,38 @@
 import { publicProcedure, router } from "@/server";
+import { z } from "zod";
 
 export const blogRouter = router({
-  get_todo: publicProcedure.query(async () => {
-    return [10, 20, 30];
+  get_all: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.blog.findMany();
   }),
-  get_books: publicProcedure.query(async () => {
-    return [10, 20, 30];
-  }),
-  get_blogs: publicProcedure.query(({ ctx }) => {
-    ctx;
-    return [10, 20, 30];
-  }),
+
+  delete: publicProcedure
+    .input(z.object({ blog_id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.blog.delete({
+        where: {
+          id: input.blog_id,
+        },
+      });
+    }),
+
+  create: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        subtitle: z.string(),
+        img_url: z.string(),
+        story: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.blog.create({
+        data: {
+          title: input.title,
+          subtitle: input.subtitle,
+          img_url: input.img_url,
+          story: input.story,
+        },
+      });
+    }),
 });
