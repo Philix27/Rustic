dfb73581345a1cebc45506dd1308e25b1cca64af
Philix27 <philixbob@gmail.com>
2023-12-17 09:50:04 +0100

    bookChapterTopics router added

--- server/routers/appRouter.ts
+++ server/routers/appRouter.ts
@@ -2,12 +2,14 @@ import { publicProcedure, router } from "../init";
 import { blogRouter } from "./blogs";
 import { booksRouter } from "./book";
 import { bookChaptersRouter } from "./bookChapter";
+import { bookChapterTopicsRouter } from "./bookChapterTopic";
 import { quizRouter } from "./quiz";
 
 export const appRouter = router({
   blog: blogRouter,
   books: booksRouter,
   bookChaptersRouter: bookChaptersRouter,
+  bookChapterTopicsRouter: bookChapterTopicsRouter,
   quiz: quizRouter,
 });
 
--- server/routers/bookChapter.ts
+++ server/routers/bookChapter.ts
@@ -11,6 +11,18 @@ export const bookChaptersRouter = router({
         },
       });
     }),
+  get_with_topics: publicProcedure
+    .input(z.object({ book_id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.prisma.bookChapter.findMany({
+        where: {
+          book_id: input.book_id,
+        },
+        select: {
+          BookChapterTopic: true,
+        },
+      });
+    }),
   get_by_id: publicProcedure
     .input(z.object({ chapter_id: z.string() }))
     .query(async ({ ctx, input }) => {
