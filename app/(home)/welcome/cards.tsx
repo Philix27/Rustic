df3fc3d84ee17270f28d8c72d4637da038724848
Philix27 <philixbob@gmail.com>
2023-12-22 21:47:02 +0100

    Made hero section responsive

--- app/(home)/welcome/cards.tsx
+++ app/(home)/welcome/cards.tsx
@@ -8,30 +8,28 @@ import Link from "next/link";
 export function CardsSection() {
   return (
     <WrapperContent>
-      <div className={"grid grid-cols-4 w-full my-12 gap-x-8 gap-y-8 "}>
+      <div className={"grid_container"}>
         {navLinkList.map((v, i) => (
           <Link href={v.link} key={i}>
             <div
               style={{ backgroundColor: AppStyles.colors.backgroundDark }}
-              className="h-[250px] flex flex-col justify-between"
+              className="item"
             >
-              <div className="flex items-center justify-center w-full pt-8 pb-4 ">
-                <div
-                  style={{ backgroundColor: v.color }}
-                  className={`text-white text-[20px] flex items-center justify-center h-[50px] w-[50px] rounded-[25px]`}
-                >
+              <div className="icon_wrapper">
+                <div style={{ backgroundColor: v.color }} className="icon">
                   {v.icon}
                 </div>
               </div>
 
-              <div className="px-5 py-3">
-                <Text variant={"B3"} className="mb-3 font-semibold text-white">
+              <div className="text_wrapper">
+                <Text variant={"B3"} className="title">
                   {v.subtitle}
                 </Text>
                 <Text variant={"B4"}>{v.story}</Text>
               </div>
+
               <div
-                className={`p-3 flex items-center justify-center`}
+                className={"footer"}
                 style={{ backgroundColor: AppStyles.colors.backgroundLight }}
               >
                 <Text variant={"B4"}>{v.title}</Text>
@@ -44,26 +42,66 @@ export function CardsSection() {
   );
 }
 
-const Wrapper = styled.div`
-  background-color: ${AppStyles.colors.background};
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-`;
-
 const WrapperContent = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
   width: 80%;
-  /* background-color: #2b1e07; */
   align-items: center;
-  /* justify-content: center; */
   padding: 50px 0;
   @media ${device.sm} {
-    background-color: #2b1e07;
+    width: 90%;
+  }
+
+  .grid_container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 15px;
+    width: 100%;
+    margin: 0 30px;
+
+    @media ${device.sm} {
+      grid-template-columns: 1fr;
+      width: 80%;
+    }
+
+    .item {
+      height: 250px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .icon_wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        padding-top: 32px;
+        padding-bottom: 16px;
+        .icon {
+          color: white;
+          font-size: 20px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 50px;
+          width: 50px;
+          border-radius: 25px;
+        }
+      }
+      .text_wrapper {
+        padding: 10px 20px;
+        .title {
+          margin-bottom: 12px;
+          font-weight: 600;
+          color: white;
+        }
+      }
+      .footer {
+        padding: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
   }
 `;
